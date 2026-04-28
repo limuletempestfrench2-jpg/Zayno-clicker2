abca360d5d73ee880fbbecffa60273bfacecb382
limuletempestfrench2-jpg <limuletempestfrench2@gmail.com>
2026-04-28 19:53:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let score = 0;
+let blood = 0;
+let bloodPerClick = 1;
+let bloodPerSecond = 0;
 
+const score = document.getElementById("score");
+const bps = document.getElementById("bps");
+const bpc = document.getElementById("bpc");
 const minos = document.getElementById("minos");
-const scoreDisplay = document.getElementById("score");
 const sound = document.getElementById("clickSound");
 
+// clic
 minos.addEventListener("click", () => {
-  score++;
-  scoreDisplay.textContent = score;
+  blood += bloodPerClick;
+  score.textContent = blood;
 
-  sound.currentTime = 0; // rejoue direct
+  sound.currentTime = 0;
   sound.play();
 });
+
+// génération automatique (sang/sec)
+setInterval(() => {
+  blood += bloodPerSecond;
+  score.textContent = blood;
+}, 1000);
+
+// affichage stats (si tu veux upgrades plus tard)
+bps.textContent = bloodPerSecond;
+bpc.textContent = bloodPerClick;
