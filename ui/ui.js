040c2903e9f6384bf4c298f3c73d4c226134b3f2
limuletempestfrench2-jpg <limuletempestfrench2@gmail.com>
2026-04-30 17:52:34 +0200

    Delete ui directory

--- ui/ui.js
+++
@@ -1,8 +0,0 @@
-import { player } from "../core/player.js";
-
-export function updateUI() {
-  document.getElementById("blood").textContent = Math.floor(player.blood);
-  document.getElementById("bpc").textContent = player.bpc;
-  document.getElementById("bps").textContent = player.bps;
-  document.getElementById("ascCost").textContent = player.ascCost;
-}
